4f04a2ec7db6ce1e65991dbb4181c4782047ae66
unknown <yashgoswami2299@gmail.com>
2022-11-13 11:16:06 +0530

    comment test files

--- src/test/java/com/maffooz/UserServiceApplicationTests.java
+++ src/test/java/com/maffooz/UserServiceApplicationTests.java
@@ -1,13 +1,13 @@
-package com.maffooz;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class UserServiceApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.maffooz;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class UserServiceApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
